d5022aea015efc58b81a863a984e818ae668bd01
schwartz <gtfsolo@gmail.com>
2024-11-18 13:15:50 +0200

    syntax jsx basic 005

--- cra/src/index.js
+++ cra/src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+const elemReact  = React.createElement('p', null, 'React разработан компанией facebook');
+
+const txt = 'Большой';
+
+const title = (
+  <>
+    <h1> Привет мир ! </h1>
+    <h2> Второго уровня {txt}</h2>
+    <div>
+      {elemReact}
+    </div>
+  </>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
+    {title}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
