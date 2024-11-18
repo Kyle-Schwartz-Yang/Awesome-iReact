8ac749fb6a881f80df88aa2e7131d6f263b83adf
schwartz <gtfsolo@gmail.com>
2024-11-18 19:38:36 +0200

    syntax 006 Fn Components

--- cra/src/index.js
+++ cra/src/index.js
@@ -5,27 +5,10 @@ import App from './App';
 
 
 
-
-const elemReact  = React.createElement('p', null, 'React разработан компанией facebook');
-
-const txt = 'Большой';
-
-const title = (
-  <>
-    <h1> Привет мир ! </h1>
-    <h2> Второго уровня {txt}</h2>
-    <div>
-      {elemReact}
-    </div>
-  </>
-);
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <App />
-    {title}
   </React.StrictMode>
 );
