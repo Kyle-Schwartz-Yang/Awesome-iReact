25d18cad799dc89dffe243e4458996ce02569938
schwartz <gtfsolo@gmail.com>
2024-11-24 22:07:14 +0200

    Serch Employes

--- icra/src/index.js
+++ icra/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components-020-exp/app/app';
+import App from './components-021/app/app';
 
 
 
